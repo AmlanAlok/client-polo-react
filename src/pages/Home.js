9f0fd0da03f7557ccfa69b0acff8aa3f24d8578a
Amlan Alok <amlanalok@gmail.com>
2023-10-14 20:24:11 -0400

    added getAll API call

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+
+    const [students, setStudents] = useState([])
+
+    useEffect(() => {
+        loadStudents()
+    }, [])
+
+    // [] will make the page load only once
+
+    const loadStudents = async () => {
+        const result = await axios.get('http://localhost:8080/student/getAll')
+        setStudents(result.data)
+    }
+
     return (
         <div className='container'>
             <div className='py-4'>
                 <table className="table border shadow">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
+                            <th scope="col">ID</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
+                        {
+                            students.map((student, index) =>(
+                                <tr>
+                                    <th scope="row" key={index}> {index + 1} </th>
+                                    <td>{student.firstName}</td>
+                                    <td>{student.lastName}</td>
+                                    <td>{student.age}</td>
+                                    <td>
+                                        <button className="btn btn-primary mx-2">View</button>
+                                        <button className="btn btn-outline-primary mx-2">Edit</button>
+                                        <button className="btn btn-danger mx-2">Delete</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                        
+                        
                     </tbody>
                 </table>
             </div>
