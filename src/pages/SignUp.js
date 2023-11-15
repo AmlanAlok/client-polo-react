f31cc31ba8bc42dc1de7ea2da3938c0d183ccca8
Amlan Alok <amlanalok@gmail.com>
2023-11-14 23:41:23 -0500

    singup 3 fields added

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -17,7 +17,7 @@ export default function SignUp() {
 
     const [pwd, setPwd] = useState('')
     const [validPwd, setValidPwd] = useState(false)
-    const [pwdFocus, setPwdfocus] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
 
     const [matchPwd, setMatchPwd] = useState('')
     const [validMatch, setValidMatch] = useState(false)
@@ -55,13 +55,30 @@ export default function SignUp() {
         setErrMsg('')
     }, [user, pwd, matchPwd])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const v1 = USER_REGEX.test(user)
+        const v2 = PWD_REGEX.test(pwd)
+        if (!v1 || !v2){
+            setErrMsg('Invalid Entry')
+            return
+        }
+        console.log(user, pwd)
+        setSuccess(true)
+    }
+
     return (
         <div>
             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live='assertive'>
                 {errMsg}
             </p>
             <h1>Register</h1>
-            <form>
+            
+            <form onSubmit={handleSubmit}>
+
+                {/* ==================== USERNAME ======================= */}
+
                 <label htmlFor="username">
                     Username:
                     <span className={validName ? 'valid' : 'hide'}>
@@ -76,6 +93,7 @@ export default function SignUp() {
                     ref={userRef}       // sets focus on the input
                     autoComplete='off'
                     onChange={(e) => setUser(e.target.value)}   // ties the input to user state
+                    value={user}
                     required
                     aria-onInvalid={validName ? 'false' : 'true'}   // Accessible Rich Internet Applications ( ARIA )
                     aria-describedby='uidnote'                      // This value be read by a screen-reader
@@ -93,7 +111,78 @@ export default function SignUp() {
                     Must begin with a letter <br />
                     Letters, numbers, underscores, hypens allowed.
                 </p>
+
+
+                {/* ==================== PASSWORD ======================= */}
+
+                <label htmlFor="password">
+                    Password:
+                    <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
+                    <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
+                </label>
+
+                <input type="password"
+                    id='password'       // should match htmlFor in the label
+                    onChange={(e) => setPwd(e.target.value)}   // ties the input to user state
+                    value={pwd}
+                    required
+                    aria-onInvalid={validPwd ? 'false' : 'true'}   // Accessible Rich Internet Applications ( ARIA )
+                    aria-describedby='pwdnote'                      // This value be read by a screen-reader
+                    onFocus={() => setPwdFocus(true)}
+                    onBlur={() => setPwdFocus(false)}
+                />
+
+                {/* This will display only if:
+                1. pwd is on focus
+                2. pwd validity is false */}
+                <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    8 to 24 characters.<br />
+                    Must include uppercase and lowercase letters, a number and a special character.<br />
+                    Allowed special characters: <span aria-label="exclamation mark">!</span>
+                    <span aria-label="at symbol">@</span>
+                    <span aria-label="hashtag">#</span>
+                    <span aria-label="dollar sign">$</span>
+                    <span aria-label="percent">%</span>
+                </p>
+
+                {/* ==================== CONFIRM PASSWORD ======================= */}
+
+                <label htmlFor="confirm_pwd">
+                    Confirm Password:
+                    <FontAwesomeIcon icon={faCheck} className={validMatch && matchPwd ? "valid" : "hide"} />
+                    <FontAwesomeIcon icon={faTimes} className={validMatch || !matchPwd ? "hide" : "invalid"} />
+                </label>
+
+                <input
+                    type="password"
+                    id="confirm_pwd"
+                    onChange={(e) => setMatchPwd(e.target.value)}
+                    value={matchPwd}
+                    required
+                    aria-invalid={validMatch ? "false" : "true"}
+                    aria-describedby="confirmnote"
+                    onFocus={() => setMatchFocus(true)}
+                    onBlur={() => setMatchFocus(false)}
+                />
+
+                <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    Must match the first password input field.
+                </p>
+
+                {/* You do not have to add type='submit' when there is only one button on the form */}
+                <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
+
             </form>
+
+            <p>
+                Already registered?<br />
+                <span className="line">
+                    {/*put router link here*/}
+                    <a href="#">Sign In</a>
+                </span>
+            </p>
         </div>
     )
 }
